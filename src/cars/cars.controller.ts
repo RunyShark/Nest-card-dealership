08518fb15d5712215a40287b8b1e1820bf495868
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-08-04 18:41:47 -0500

    params

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 @Controller('cars')
 export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Audi', 'Mustang', 'Mercedez'];
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCardById(@Param('id') id) {
+    return {
+      car: this.carsService.findAll()[+id],
+    };
   }
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+// export interface Cars {
+//   id: number;
+//   brand: string;
+//   model: string;
+// }
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'Ford',
+      model: 'Mustang',
+    },
+    {
+      id: 3,
+      brand: 'Lambrohini',
+      model: 'Infirno',
+    },
+  ];
+  findAll() {
+    return this.cars;
+  }
+}
