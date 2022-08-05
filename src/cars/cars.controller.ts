f1f3724746cab9d4d4a58f88b28aec64fb2a28c5
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-08-04 19:21:25 -0500

    CRUD

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
+export interface Cars {
+  id: number;
+  brand: string;
+  model: string;
+}
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
@@ -14,5 +28,20 @@ export class CarsController {
     return {
       car: this.carsService.findOnenById(+id),
     };
+  }
+
+  @Post()
+  AddNewCar(@Body() car: any) {
+    return car;
+  }
+
+  @Patch(':id')
+  UpdateCar(@Body() id: number, car: Cars) {
+    return 'Patch';
+  }
+
+  @Delete(':id')
+  DeleteCar(@Param('id', ParseIntPipe) id: number) {
+    return 'Delete';
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
-// export interface Cars {
-//   id: number;
-//   brand: string;
-//   model: string;
-// }
+import { Injectable, NotFoundException } from '@nestjs/common';
+export interface Cars {
+  id: number;
+  brand: string;
+  model: string;
+}
 @Injectable()
 export class CarsService {
   private cars = [
@@ -29,6 +29,23 @@ export class CarsService {
 
   findOnenById(id: number) {
     const car = this.cars.find((car) => car.id === id);
+
+    if (!car) throw new NotFoundException(`Card with id  '${id}' not found`);
+
     return car;
+  }
+
+  AddCar(car: Cars) {
+    const newCar = this.cars.push(car);
+    return newCar;
+  }
+
+  putCar(id: number, car: Cars) {
+    const editCar = this.cars.find((car) => car.id === id);
+    return editCar;
+  }
+
+  deleteCar(id: number) {
+    this.cars.slice(id);
   }
 }
