4bd275e69a6642f02c2eabcc12efc32be731dbb3
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-08-05 15:23:24 -0500

    modulesimports

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from '../cars/cars.service';
+import { BrandsService } from '../brands/brands.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly CarsService: CarsService,
+    private readonly BrandsService: BrandsService,
+  ) {}
   populateDb() {
+    this.CarsService.fillCarsWithSeedData(CARS_SEED);
+    this.BrandsService.fillBrandsWithSeedData(BRANDS_SEED);
     return 'Seed execute';
   }
 }
